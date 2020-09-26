0cef66d1480f13505f32cbda171889272f5bc822
zergaj <7532332+zergaj@users.noreply.github.com>
2020-09-26 14:09:08 +0300

    Update ChislaFibbonachi.py
    рабочее решение с while-циклом

--- ChislaFibbonachi.py
+++ ChislaFibbonachi.py
@@ -6,3 +6,27 @@ while n > 1:
     f =             # здесь должен быть норм код
     n = n - 1
 print(f)
+
+
+n = int(input())
+
+f0 = 0
+f1 = 1
+
+if n < 0:
+    print('Error, n must be greater then 0')
+elif n == 0:
+    print('Fibonacci is {f}'.format(f=f0))
+elif n == 1:
+    print('Fibonacci is {f}'.format(f=f1))
+else:
+    f_prev_prev = f0
+    f_prev = f1
+    f_result = 0
+    while n > 1:
+        f_result = f_prev + f_prev_prev
+        f_prev_prev = f_prev
+        f_prev = f_result
+        n = n - 1
+
+    print('Fibonacci is {f}'.format(f=f_result))
